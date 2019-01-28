2c785906fbdd92d6c7265cc115238dc64d30c93f
Hale III, Edward <edward.hale@tyson.com>
2019-01-28 10:48:58 -0600

    Adding for deployment to personal account

--- commands.sh
+++ commands.sh
@@ -1,8 +1,8 @@
 #create s3 bucket for packaged config
-aws s3 mb s3://<insert-bucket-name>
+aws s3 mb s3://sam-bucket
 
 #package the template file and output
-aws cloudformation package --s3-bucket <bucket-name> --template-file template.yaml --output-template-file serverless-output.yaml 
+aws cloudformation package --s3-bucket sam-bucket --template-file template.yaml --output-template-file serverless-output.yaml 
 
 #deploy to cloudformation
-aws cloudformation deploy --template-file serverless-output.yaml  --stack-name <stack-name> --capabilities CAPABILITY_IAM
+aws cloudformation deploy --template-file serverless-output.yaml  --stack-name graphql-test-stack --capabilities CAPABILITY_IAM
